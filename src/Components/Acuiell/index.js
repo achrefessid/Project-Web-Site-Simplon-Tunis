3879b0c777acf76feedfe6191090cd3236c2b40f
achref essid <essid.developpeur@gmail.com>
2020-05-06 21:34:44 +0100

    Remove Empty Components

--- src/Components/Acuiell/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Acuiell = () => {                               
-    return (
-  <h1>Acuiell</h1>
-    )
-  }
-  
-export default Acuiell; 
--- src/Components/Apropos/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Apropos = () => { 
-    return (
-    <h1>A propos</h1>
-    )
-  }
-
-export default Apropos; 
